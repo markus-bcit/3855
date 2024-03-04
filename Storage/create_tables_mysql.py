08ed2131b81a75662e78d1cdd0da81160e7e7949
Markus Afonso <mafonso4@my.bcit.ca>
2024-03-04 12:13:50 -0800

    auth

--- Storage/create_tables_mysql.py
+++ Storage/create_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
-db_conn = mysql.connector.connect(host="markus-kafka.canadacentral.cloudapp.azure.com",port=3306, user="user",
-                                  password="password", database="events", auth_plugin='mysql_native_password')
+db_conn = mysql.connector.connect(host="markus-kafka.canadacentral.cloudapp.azure.com", port=3306, user="user",
+                                  password="password", database="events")
 db_cursor = db_conn.cursor()
 db_cursor.execute('''
           CREATE TABLE workout
