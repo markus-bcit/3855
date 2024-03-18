d372f2f654493d6731ca23d3d2e5b5af5a4e252b
Markus Afonso <mafonso4@my.bcit.ca>
2024-03-18 15:04:14 -0700

    idk anymore

--- Audit/app.py
+++ Audit/app.py
@@ -1,7 +1,5 @@
 from connexion import NoContent
 from pykafka import KafkaClient
-from connexion.middleware import MiddlewarePosition
-from starlette.middleware.cors import CORSMiddleware
 
 import requests
 import json
@@ -87,14 +85,6 @@ def get_workout_log(index):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
-app.add_middleware(
-    CORSMiddleware,
-    position=MiddlewarePosition.BEFORE_EXCEPTION,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 if __name__ == "__main__":
     app.run(port=8110)
