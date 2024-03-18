e9f2a97138d33550f6d39d92b9a5f5000081797b
markus <mafonso4@my.bcit.ca>
2024-03-17 18:49:12 -0700

    adds cors flask

--- Audit/app.py
+++ Audit/app.py
@@ -1,7 +1,6 @@
 from connexion import NoContent
 from pykafka import KafkaClient
-from connexion.middleware import MiddlewarePosition
-from starlette.middleware.cors import CORSMiddleware
+from flask_cors import CORS
 
 import requests
 import json
@@ -87,14 +86,7 @@ def get_workout_log(index):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
-app.app.add_middleware(
-    CORSMiddleware,
-    position=MiddlewarePosition.BEFORE_EXCEPTION,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+CORS(app.app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(port=8110)
