94c706831db734687393a51e1ed03c56f394a4ce
markus <mafonso4@my.bcit.ca>
2024-04-02 15:39:54 -0700

    Updates receiver to storage a message upon succesful start

--- Storage/app.py
+++ Storage/app.py
@@ -125,8 +125,7 @@ def create_kafka_client():
             logging.info(
                 f"Attempting to connect to Kafka, retry {retry_count}")
             client = KafkaClient(hosts=hostname)
-            topic = client.topics[str.encode(app_config["events"]["topic"])]
-            return client, topic
+            return client
         except Exception as e:
             logging.error(f"Failed to connect to Kafka: {e}")
             time.sleep(5)
@@ -134,34 +133,40 @@ def create_kafka_client():
     raise Exception("Failed to connect to Kafka after maximum retries")
 
 
+def publish_ready_message():
+    try:
+        client = create_kafka_client()
+        topic = client.topics[str.encode('event_log')]
+        producer = topic.get_sync_producer()
+
+        ready_msg = {
+            "type": "startup",
+            "message": "Receiver is ready to consume messages from the events topic",
+            "code": "0002"
+        }
+        ready_msg_str = json.dumps(ready_msg)
+
+        producer.produce(ready_msg_str.encode('utf-8'))
+        logger.info('Published message to event_log topic: %s', ready_msg_str)
+    except Exception as e:
+        logger.error('Error publishing message to event_log topic: %s', str(e))
+
+
 def process_messages():
-    """ Process event messages """
-    client, topic = create_kafka_client()
-    # hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
-    # client = KafkaClient(hosts=hostname)
+    try:
+        client = create_kafka_client()
+        topic = client.topics[str.encode(app_config["events"]["topic"])]
 
-    # Create a consume on a consumer group, that only reads new messages
-    # (uncommitted messages) when the service re-starts (i.e., it doesn't
-    # read all the old messages from the history in the message queue).
-    consumer = topic.get_simple_consumer(consumer_group=b'event_group',
-                                         reset_offset_on_start=False,
-                                         auto_offset_reset=OffsetType.LATEST)
-    # This is blocking - it will wait for a new message
-    for msg in consumer:
-        msg_str = msg.value.decode('utf-8')
-        msg = json.loads(msg_str)
-        logger.info("Message: %s", msg)
-        payload = msg["payload"]
-        if msg["type"] == "workout":  # Change this to your event type
-            # Store the event1 (i.e., the payload) to the DB
-            create_workout(payload)
-        elif msg["type"] == "workoutlog":  # Change this to your event type
-            # Store the event2 (i.e., the payload) to the DB
-            log_workout(payload)
-
-        # Commit the new message as being read
-        consumer.commit_offsets()
-
+        consumer = topic.get_simple_consumer(consumer_group=b'event_group',
+                                             reset_offset_on_start=False,
+                                             auto_offset_reset=OffsetType.LATEST)
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            logger.info("Message: %s", msg)
+            consumer.commit_offsets()
+    except Exception as e:
+        logger.error('Error processing messages: %s', str(e))
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
@@ -171,4 +176,6 @@ if __name__ == "__main__":
     t1 = Thread(target=process_messages, daemon=True)
     t1.start()
 
-    app.run(port=8090)
+    publish_ready_message()
+
+    app.run(port=8080)
