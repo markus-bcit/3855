4ba079d800dd12dc04b384c969f89191fce0bfbc
markus <mafonso4@my.bcit.ca>
2024-04-02 17:39:57 -0700

    Updates processing

--- Processing/app.py
+++ Processing/app.py
@@ -180,4 +180,20 @@ app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     init_scheduler()
+    try:
+        client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
+        topic = client.topics[str.encode(app_config['events']['topic2'])]
+        producer = topic.get_sync_producer()
+
+        ready_msg = {
+            "type": "startup",
+            "message": "Processing is ready to receive messages on its RESTful API",
+            "code": "0003"
+        }
+        ready_msg_str = json.dumps(ready_msg)
+
+        producer.produce(ready_msg_str.encode('utf-8'))
+        logger.info('Published message to event_log topic: %s', ready_msg_str)
+    except Exception as e:
+        logger.error('Error publishing message to event_log topic: %s', str(e))
     app.run(port=8100, host='0.0.0.0')
