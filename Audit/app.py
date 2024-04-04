cbd99116062f6cf55972b6b9ba175a1c72b94d7e
Markus Afonso <mafonso4@my.bcit.ca>
2024-04-04 10:44:46 -0700

    Updates processing and audit to have enable cors when not in test env.

--- Audit/app.py
+++ Audit/app.py
@@ -97,8 +97,9 @@ def get_workout_log(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml", base_path="/audit_log", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
