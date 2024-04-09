3654b7cc04cae3541cee19786e1f2ff142112162
Markus Afonso <mafonso4@my.bcit.ca>
2024-04-09 11:47:39 -0700

    Cors recevier

--- Receiver/app.py
+++ Receiver/app.py
@@ -1,6 +1,6 @@
 from connexion import NoContent
 from pykafka import KafkaClient
-
+from flask_cors import CORS
 import requests
 import json
 import connexion
@@ -90,6 +90,9 @@ def log_workout(body):
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", base_path="/receiver", strict_validation=True,validate_responses=True)
 
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     try:
