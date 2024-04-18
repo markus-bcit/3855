7667b9494a02e28305a838142163fb730e859bf6
Markus Afonso <mafonso4@my.bcit.ca>
2024-04-18 15:00:55 -0700

    init final

---
+++ Anomalies/app.py
@@ -0,0 +1,152 @@
+import yaml
+import logging
+import logging.config
+import uuid
+import requests
+import datetime
+import time
+import json
+
+import connexion
+from threading import Thread
+from pykafka import KafkaClient
+from pykafka.common import OffsetType
+from connexion import NoContent
+from pykafka import KafkaClient
+from starlette.middleware.cors import CORSMiddleware
+from flask_cors import CORS
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from base import Base
+from workout_stats import Anomaly
+from base import Base
+from apscheduler.schedulers.background import BackgroundScheduler
+
+import pytz
+timezone = pytz.timezone('America/Los_Angeles')
+
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+DB_ENGINE = create_engine(f"sqlite:///{app_config['datastore']['filename']}")
+Base.metadata.bind = DB_ENGINE
+DB_SESSION = sessionmaker(bind=DB_ENGINE)
+
+
+def get_anomaly_stats():
+    logger.info("Request for event logger has started")
+
+    session = DB_SESSION()
+
+    current_anomaly = session.query(Anomaly).order_by(
+        Anomaly.last_update.desc()).first()
+
+    if current_anomaly:
+        logger.debug("Current statistics: %s", current_anomaly.to_dict())
+        logger.info("Request for statistics has completed")
+        session.close()
+        return current_anomaly.to_dict(), 200
+    else:
+        logger.error("Statistics do not exist")
+        logger.info("Request for statistics has completed")
+        session.close()
+        return "Statistics do not exist", 404
+
+
+def populate_anomaly():
+    logger.info("Periodic processing for event logger has started")
+
+    session = DB_SESSION()
+
+    event_id = ''
+    trace_id = ''
+    event_type = ''
+    anomaly_type = ''
+    description = ''
+
+    client = create_kafka_client()
+    topic = client.topics[str.encode(app_config["events"]["topic"])]
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    logger.info("Retrieving Event Logger")
+    try:
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            if msg.get('type') == 'workout':
+                payload = msg.get('payload')
+                if payload.get('frequency') > app_config["threshold"]["workout"]:
+                    event_id = payload.get('eventId')
+                    trace_id = payload.get('traceId')
+                    event_type = 'workout'
+                    description = f"Workout frequency of {payload.get('frequency')} greater than threshold of {app_config['threshold']['workout']}"
+                    anomaly_type = 'workout'
+            elif msg.get('type') == 'workoutlog':
+                payload = msg.get('payload')
+                if payload.get('frequency') > app_config["threshold"]["workout"]:
+                    
+                    exercises = json.loads(payload.get('exercises'))
+                    
+                    event_id = payload.get('eventId')
+                    trace_id = payload.get('traceId')
+                    event_type = 'workout'
+                    description = f"Excersice count of {len(exercises)} greater than threshold of {app_config['threshold']['workout']}"
+                    anomaly_type = 'workout'
+            logger.info(f"Consumed Code: {msg.get('code')} Message: {msg.get('message')}")
+    except:
+        logger.error("No more messages found")
+
+    new_anomaly = Anomaly(
+        event_id=event_id,
+        trace_id=trace_id,
+        event_type=event_type,
+        description=description,
+        anomaly_type=anomaly_type
+    )
+    session.add(new_anomaly)
+    session.commit()
+
+    logger.debug("Updated statistics ID: %s", new_anomaly.id)
+
+    session.close()
+
+    logger.info("Periodic processing has ended")
+
+
+def create_kafka_client():
+    max_retries = app_config['kafka']['max_retries']
+    retry_count = 0
+    hostname = f"{app_config['events']['hostname']}:{app_config['events']['port']}"
+    while retry_count < max_retries:
+        try:
+            logging.info(
+                f"Attempting to connect to Kafka, retry {retry_count}")
+            client = KafkaClient(hosts=hostname)
+            return client
+        except Exception as e:
+            logging.error(f"Failed to connect to Kafka: {e}")
+            time.sleep(5)
+            retry_count += 1
+    raise Exception("Failed to connect to Kafka after maximum retries")
+
+
+def init_scheduler():
+    sched = BackgroundScheduler(daemon=True, timezone='America/Los_Angeles')
+    sched.add_job(populate_anomaly, 'interval', seconds=5)
+    sched.start()
+
+
+# Initialize the Flask app
+app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
+app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+
+if __name__ == "__main__":
+    init_scheduler()
+    app.run(port=8130, host='0.0.0.0')
