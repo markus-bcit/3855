a6d813169a30c1dfa5383ace8233342503a874dc
Markus Afonso <mafonso4@my.bcit.ca>
2024-04-18 16:26:46 -0700

    a

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,32 +1,36 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
-import EndpointAudit from './components/EndpointAudit'
-import AppStats from './components/AppStats'
-import EventLogger from './components/EventLogger'
+import EndpointAudit from "./components/EndpointAudit";
+import AppStats from "./components/AppStats";
+import EventLogger from "./components/EventLogger";
+import Anomalies from "./components/Anomalies";
 
 function App() {
+  const endpoints = ["workout/log", "workout"];
 
-    const endpoints = ["workout/log", "workout"]
+  const rendered_endpoints = endpoints.map((endpoint) => {
+    return <EndpointAudit key={endpoint} endpoint={endpoint} />;
+  });
 
-    const rendered_endpoints = endpoints.map((endpoint) => {
-        return <EndpointAudit key={endpoint} endpoint={endpoint}/>
-    })
-
-    return (
-        <div className="App">
-            <img src={logo} className="App-logo" alt="logo" height="150px" width="400px"/>
-            <div>
-                <AppStats/>
-                <h1>Audit Endpoints</h1>
-                {rendered_endpoints}
-                <EventLogger/>
-            </div>
-        </div>
-    );
-
+  return (
+    <div className="App">
+      <img
+        src={logo}
+        className="App-logo"
+        alt="logo"
+        height="150px"
+        width="400px"
+      />
+      <div>
+        <AppStats />
+        <h1>Audit Endpoints</h1>
+        {rendered_endpoints}
+        <EventLogger />
+        <Anomalies />
+      </div>
+    </div>
+  );
 }
-
-
 
 export default App;
---
+++ dashboard-ui/src/components/Anomalies.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import "../App.css";
+
+export default function AppStats() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [stats, setStats] = useState({});
+  const [error, setError] = useState(null);
+
+  const getStats = () => {
+    fetch(`http://markus-kafka.canadacentral.cloudapp.azure.com/anomaly_detector/anomaly_stats`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log("Received anomaly");
+          setStats(result);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setError(error);
+          setIsLoaded(true);
+        }
+      );
+  };
+  useEffect(() => {
+    const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+    return () => clearInterval(interval);
+  }, [getStats]);
+
+  if (error) {
+    return <div className={"error"}>Error found when fetching from API</div>;
+  } else if (isLoaded === false) {
+    return <div>Loading...</div>;
+  } else if (isLoaded === true) {
+    return (
+      <div>
+        <h1>Anomalies</h1>
+        <table className={"StatsTable"}>
+          <tbody>
+            <tr>
+              <td><h3>Workout Anomalies</h3> {stats["workout_log"]}</td>
+            </tr>
+            <tr>
+              <td colspan="2"><h3>Workout Log Anomalies</h3> {stats["workout"]}</td>
+            </tr>
+            <tr>
+              <td colspan="2">
+              Total # of anomalies {stats["num_anomalies"]}
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">Description {stats["most_recent_desc"]}</td>
+            </tr>
+            <tr>
+              <td colspan="2">Last Updated: {stats["most_recent_datetime"]}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
